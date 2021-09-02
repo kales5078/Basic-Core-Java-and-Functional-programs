63043d84dc524dfd5d1ca7e948c6ac22758eab31
kales5078 <88039478+kales5078@users.noreply.github.com>
2021-09-02 18:48:44 +0530

    Add : Initial Commit

---
+++ VowelConsonant.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+	
+	Scanner sc=new Scanner(System.in);
+
+	// Get input of alphabet from user
+
+        System.out.println("Enter the alphabet");
+	char ch=sc.next().charAt(0);
+	
+	// check input is equal to a,e,i,o,u
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
